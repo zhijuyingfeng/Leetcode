74f1e99abdb73922d1b0fe6776d08709d93a84f2
zhijuyingfeng <2604385867@qq.com>
2022-08-28 21:52:53 +0800

    279. Perfect Squares

--- 279.java
+++ 279.java
@@ -4,9 +4,10 @@ class Solution {
         for (int i = 1; i <= n; i++) {
             dp[i] = i;
             for (int j = 1; j <= 100; j++) {
-                if (i - j * j >= 0) {
+                if (i - j * j >= 0)
                     dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
-                }
+                else
+                    break;
             }
         }
         return dp[n];
